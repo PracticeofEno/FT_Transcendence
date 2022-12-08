5a7e987559ede5520df3c21ccf6e1e1e4cd4ff0b
haha <haha@gmail.com>
2022-12-09 05:15:53 +0900

    aa2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,10 +14,10 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 3000,
+    port: 80,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://42-backend:5000",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "")
